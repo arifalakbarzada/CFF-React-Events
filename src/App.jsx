94d860b1b1373264bd0bbf93488b3cfabdc306dc
arif2004js <yearif689@gmail.com>
2024-04-25 15:53:25 +0400

    Task upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <button onClick={()=>{console.log("Hello, I am Arif")}}>Click me</button>
+    </div>
+  )
+}
+
+export default App
